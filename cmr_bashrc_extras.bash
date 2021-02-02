de6160427d326b73a7a76f26e8bdb7f6ab03bd60
Ben Woodcroft <benjwoodcroft@gmail.com>
2021-02-02 17:40:32 +1000

    cmr_bashrc_extras.bash: Towards cron.

--- cmr_bashrc_extras.bash
+++ cmr_bashrc_extras.bash
@@ -1,3 +1,19 @@
-
 # Add HPC scripts e.g. mqsub to the path
-export PATH=/work/microbiome/sw/hpc_scripts/bin:$PATH
+export PATH="/work2/microbiome/sw/hpc_scripts/bin:$PATH"
+
+#function for command prompt and email notification of job completion
+function notify { command "$@" && success || fail; }
+
+#setup temporary file folder # No need for this since /tmp is /data1 in mounts
+# export TMPDIR=/data1/tmp-$USER
+# mkdir -p $TMPDIR
+
+#check for nextflow config and source if none found
+if [[ ! -e ~/.nextflow/config ]]; then
+    source /work2/microbiome/sw/nextflow_config/config
+fi
+
+#re-sources cron-scheduled jobs
+#crontab -r
+#crontab /work2/microbiome/sw/hpc_scripts/bin/cron
+
