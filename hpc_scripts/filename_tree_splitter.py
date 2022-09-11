ddf2b73fd72eeb80b18ff505be73e65a542c15cd
Ben Woodcroft <benjwoodcroft@gmail.com>
2022-09-12 07:13:49 +1000

    mqsub: Add --segregated-log-files.

---
+++ hpc_scripts/filename_tree_splitter.py
@@ -0,0 +1,12 @@
+class FilenameTreeSplitter:
+    def chunks(self, split_lengths, input_filename):
+        chunks = []
+        index = 0
+        for length in split_lengths:
+            if index + length > len(input_filename):
+                break
+            chunks.append(input_filename[index:(index+length)])
+            index = index + length
+        if index < len(input_filename):
+            chunks.append(input_filename[index:])
+        return chunks
