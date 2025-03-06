8c08f9c7fa013008a71693cc0dd6c45eae819e5d
Ben Woodcroft <benjwoodcroft@gmail.com>
2025-03-06 15:49:59 +1000

    mqinteractive: New script.

--- cmr_bashrc_extras_aqua.bash
+++ cmr_bashrc_extras_aqua.bash
@@ -4,27 +4,16 @@ export PATH="/mnt/hpccs01/work/microbiome/sw/hpc_scripts/bin:$PATH"
 #function for command prompt and email notification of job completion
 function notify { command "$@" && success || fail; }
 
-#setup temporary file folder # No need for this since /tmp is /data1 in mounts
-# export TMPDIR=/data1/tmp-$USER
-# mkdir -p $TMPDIR
-
-#check for nextflow config
-if [[ ! -e ~/.nextflow/config ]]; then
-    NEXTFLOW_CONFIG=/mnt/hpccs01/work/microbiome/sw/nextflow_config/config
-fi
-
-# Otherwise qaddtime is only available on lyra # currently disabled due to abuse
-#alias qaddtime=/pkg/hpc/scripts/qaddtime
-
 # Save all the history, see https://debian-administration.org/article/543/Bash_eternal_history
 export HISTTIMEFORMAT="%F %T "
 PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND ; }"'echo $$ $USER \
                "$(history 1)" >> ~/.bash_eternal_history'
 
-#Path to kingfisher # symlinked /lustre/work-lustre/microbiome/sw/kingfisher-download/bin/kingfisher in $CONDA_PREFIX/envs/kingfisher/bin instead
-export PATH=/work/microbiome/sw/kingfisher_repos/kingfisher-v0.4.1/kingfisher-download/bin:$PATH
-export PATH=/work/microbiome/sw/recurm_repos/recurm-v0.3.0/recurm/bin:$PATH
-
 # Setup snakemake config directories so it interfaces well with the PBS system
 mkdir -p ~/.config/snakemake
 cd ~/.config/snakemake && ln -sf /work/microbiome/sw/hpc_scripts/snakemake_configs/* . && cd $OLDPWD
+
+# Setup for mqinteractive. If interactive and latest.sh exists, load 'er up.
+if [[ ${PBS_ENVIRONMENT} == "PBS_INTERACTIVE" ]] && [[ -f $HOME/.hpc_scripts/mqinteractive_scripts/latest.sh ]]; then
+    source $HOME/.hpc_scripts/mqinteractive_scripts/latest.sh
+fi
