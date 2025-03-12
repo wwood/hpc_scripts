ee3e51d87cbc4abbbbc3cc0f9b80c143e27c17b7
Ben Woodcroft <benjwoodcroft@gmail.com>
2025-03-13 07:44:18 +1000

    mqinteractive: Add gpu argument.

--- cmr_bashrc_extras.bash
+++ cmr_bashrc_extras.bash
@@ -2,11 +2,11 @@
 
 case $ID in
   sles) echo "Suse Linux" 1>&2
-    echo Running lyra cmr .bashrc extras 1>&2
+    # echo Running lyra cmr .bashrc extras 1>&2
     . /work/microbiome/sw/hpc_scripts/cmr_bashrc_extras_lyra.bash
     ;;
   rhel)
-    echo "RedHat Linux" 1>&2
+    # echo "RedHat Linux" 1>&2
     echo Running aqua cmr .bashrc extras 1>&2
     . /work/microbiome/sw/hpc_scripts/cmr_bashrc_extras_aqua.bash
     ;;
